ebf7faacbc3698d2aad6ab060c15f4a4ce05f08e
ryotaro-tanaka <ryotaro_tanaka@outlook.com>
2026-02-17 09:19:19 +1000

    bot: T002

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,6 @@
-export type GameState = {
-  turn: number;
-};
+import { GameState, Intent, Event } from './types';
+
+export { GameState, Intent, Event };
 
 export function createInitialState(): GameState {
   return { turn: 1 };
